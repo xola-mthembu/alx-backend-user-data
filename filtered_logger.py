3cfb4a8a8e29429fa1ae31c5cc3746b670b823e1
xola-mthembu <xola.mthembu@gmail.com>
2024-08-29 18:36:50 +0000

    Task 0: Implemented regex-based field obfuscation in log messages

--- filtered_logger.py
+++ filtered_logger.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
 """
-Module for handling Personal Data
+Module for filtering log messages with sensitive information.
 """
 
 import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction:
+           str, message: str, separator: str) -> str:
     """
-    Obfuscates specified fields in the log message.
+    Obfuscates specified fields in a log message.
 
     Args:
-        fields: List of strings representing fields to obfuscate
-        redaction: String to replace field values
-        message: String representing the log line
-        separator: String representing the character separating fields
+        fields (List[str]): List of fields to obfuscate.
+        redaction (str): Replacement string for obfuscated fields.
+        message (str): The original log message.
+        separator (str): The separator between fields in the log message.
 
     Returns:
-        Obfuscated log message
+        str: The obfuscated log message.
     """
     for field in fields:
-        message = re.sub(f'{field}=.*?{separator}',
-                         f'{field}={redaction}{separator}', message)
+        message = re.sub(rf"{field}=[^{separator}]*",
+                  f"{field}={redaction}", message)
     return message
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 """
-Main file for testing filter_datum function
+Main file for testing filter_datum function.
 """
 
 filter_datum = __import__('filtered_logger').filter_datum
 
 fields = ["password", "date_of_birth"]
-messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;",
-            "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
+messages = [
+    "name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;",
+    "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"
+]
 
 for message in messages:
     print(filter_datum(fields, 'xxx', message, ';'))
