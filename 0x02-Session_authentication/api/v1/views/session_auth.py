6f4ed8cb1b9568b2a8d5e03ee0e8a80aedf8a37c
xola-mthembu <xola.mthembu@gmail.com>
2024-09-05 19:41:09 +0000

    api/v1/views/session_auth.py

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+""" Module of Session Authentication views
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """ POST /api/v1/auth_session/login
+    Return:
+      - User object JSON represented
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    try:
+        users = User.search({'email': email})
+    except Exception:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    if not users:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    user = users[0]
+
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    session_name = getenv('SESSION_NAME')
+    response = jsonify(user.to_json())
+    response.set_cookie(session_name, session_id)
+
+    return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def logout():
+    """ DELETE /api/v1/auth_session/logout
+    Return:
+      - Empty JSON object
+    """
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)
